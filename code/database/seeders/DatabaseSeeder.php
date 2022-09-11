250b40e957ccab3a1bd693c55ec7d87c0c2db7ae
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-11 18:35:30 +0200

    setup

--- code/database/seeders/DatabaseSeeder.php
+++ code/database/seeders/DatabaseSeeder.php
@@ -1,19 +1,18 @@
 <?php
+    namespace Database\Seeders;
 
-namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+    use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+    use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+
+    class DatabaseSeeder
+        extends Seeder
     {
-        // $this->call('UsersTableSeeder');
+
+        public function run()
+        {
+            // $this->call('UsersTableSeeder');
+        }
     }
-}
+?>
