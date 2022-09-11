3d117fc0d1eab35e3f780341c36e420aaf010baa
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-11 21:02:28 +0200

    c

--- code/app/Providers/EventServiceProvider.php
+++ code/app/Providers/EventServiceProvider.php
@@ -1,29 +1,26 @@
 <?php
+    namespace App\Providers;
 
-namespace App\Providers;
+    use Laravel\Lumen\Providers\EventServiceProvider
+        as ServiceProvider;
 
-use Laravel\Lumen\Providers\EventServiceProvider as ServiceProvider;
 
-class EventServiceProvider extends ServiceProvider
-{
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        \App\Events\ExampleEvent::class => [
-            \App\Listeners\ExampleListener::class,
-        ],
-    ];
-
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     *
-     * @return bool
-     */
-    public function shouldDiscoverEvents()
+    class EventServiceProvider
+        extends ServiceProvider
     {
-        return false;
+
+        protected $listen =
+        [
+            \App\Events\ExampleEvent::class =>
+            [
+                \App\Listeners\ExampleListener::class,
+            ],
+        ];
+
+
+        public function shouldDiscoverEvents()
+        {
+            return false;
+        }
     }
-}
+?>
